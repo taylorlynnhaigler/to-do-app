3117cf4ba0e3525f309ade76a67efa935d0a4510
tay <taylorlynn@haigler.net>
2019-01-16 09:55:41 -0600

    Checkpoint 6 dom: completed

---
+++ app2.js
@@ -0,0 +1,44 @@
+function onReady() {
+ const toDos = [];
+ const addToDoForm = document.getElementById('addToDoForm');
+
+ function createNewToDo() {
+   const newToDoText = document.getElementById('newToDoText');
+   if (!newToDoText.value) { return; }
+   toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+     event.preventDefault();
+     createNewToDo();
+   });
+
+   renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
